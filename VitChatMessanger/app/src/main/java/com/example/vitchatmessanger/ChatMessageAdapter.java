d50dc3a84bd9c4c4797bd6434eb7d478cf5a9759
VitaliyOleinik <oleynik624@gmail.com>
2021-07-02 18:43:42 +0600

    activities, adapter, entity

---
+++ VitChatMessanger/app/src/main/java/com/example/vitchatmessanger/ChatMessageAdapter.java
@@ -0,0 +1,56 @@
+package com.example.vitchatmessanger;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
+    public ChatMessageAdapter(@NonNull Context context, int resource, List<ChatMessage> messages) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if(convertView == null){
+            convertView = ((Activity)getContext()).getLayoutInflater()
+                    .inflate(R.layout.message_item, parent, false);
+        }
+
+        ImageView photoImageView = convertView.findViewById(R.id.photoImageView);
+        TextView textTextView = convertView.findViewById(R.id.textTextView);
+        TextView nameTextView = convertView.findViewById(R.id.nameTextView);
+
+        ChatMessage message = getItem(position);
+
+        boolean isText = message.getImageUrl() == null;
+
+        if(isText){
+            textTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            textTextView.setText(message.getText());
+        } else {
+            textTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView.getContext())
+                    .load(message.getImageUrl())
+                    .into(photoImageView);
+        }
+
+        nameTextView.setText(message.getName());
+
+        return convertView;
+    }
+}
