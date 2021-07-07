5d890df4b267a3ce864ef1d66ca79d844ad5a6c5
VitaliyOleinik <oleynik624@gmail.com>
2021-07-07 20:09:05 +0600

    user adapter

--- VitChatMessanger/app/src/main/java/com/example/vitchatmessanger/User.java
+++ VitChatMessanger/app/src/main/java/com/example/vitchatmessanger/User.java
@@ -4,14 +4,24 @@ class User {
     private String name;
     private String email;
     private String id;
+    private int avatarMockUpResource;
 
     public User() {
     }
 
-    public User(String name, String email, String id) {
+    public User(String name, String email, String id, int avatarMockUpResource) {
         this.name = name;
         this.email = email;
         this.id = id;
+        this.avatarMockUpResource = avatarMockUpResource;
+    }
+
+    public int getAvatarMockUpResource() {
+        return avatarMockUpResource;
+    }
+
+    public void setAvatarMockUpResource(int avatarMockUpResource) {
+        this.avatarMockUpResource = avatarMockUpResource;
     }
 
     public String getName() {
