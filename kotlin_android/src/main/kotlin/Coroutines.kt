a5c0c54d654f379226f3305ffa32a4e537091a57
VitaliyOleinik <oleynik624@gmail.com>
2021-07-29 12:03:52 +0600

    work with coroutines, rewrite from thread

---
+++ kotlin_android/src/main/kotlin/Coroutines.kt
@@ -0,0 +1,17 @@
+import kotlinx.coroutines.*
+
+class Coroutines {
+    fun main() {
+        val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+        repeat(3) {
+            GlobalScope.launch {
+                println("${Thread.currentThread()} has started")
+                for (i in states) {
+                    println("${Thread.currentThread()} - $i")
+                    delay(5000)
+                }
+            }
+        }
+    }
+
+}
