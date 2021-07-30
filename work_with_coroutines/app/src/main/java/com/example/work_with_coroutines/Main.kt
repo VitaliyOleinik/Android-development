c5b32ac757581ea67020d867345f44a7956ed058
VitaliyOleinik <oleynik624@gmail.com>
2021-07-30 11:12:42 +0600

    suspend functions, runBlocking()

--- work_with_coroutines/app/src/main/java/com/example/work_with_coroutines/Main.kt
+++ work_with_coroutines/app/src/main/java/com/example/work_with_coroutines/Main.kt
@@ -3,14 +3,38 @@ package com.example.work_with_coroutines
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 private fun log(msg : String) = println(
     "[${Thread.currentThread().name}] $msg"
 )
 
 fun main() {
-    networkRequest()
+    //networkRequest()
+
+    runBlocking {
+        launch {
+            makeAPIRequest()
+        }
+        log("This will be printed first")
+    }
+    log("Finished!")
 }
+
+
+private suspend fun makeAPIRequest () {
+    checkForInternet()
+    delay(1000)
+    log("API request failed because of missing internet.")
+}
+
+private suspend fun checkForInternet(){
+    delay(3000)
+    log("There is internet")
+    delay(1000)
+    log("Just kidding, there is no internet")
+}
+
 
 private fun networkRequest(){
     GlobalScope.launch {
